05daa7e7ca4a00b96d0b644a0c38df99771f5339
josehomero <jose.homero.rios@outlook.com>
2020-05-27 20:32:23 -0700

    updated

--- script.js
+++ script.js
@@ -3,28 +3,37 @@ const canvasContext = canvas.getContext('2d')
 canvas.style.marginLeft = '30%';
 canvas.style.background = 'gray';
 let snakeMotion = 10; // to move the snake on the x-axis
-let snakeMoveLeft = false; // me attempting to move the snake left
+let snakeMoveLeft = 0; // me attempting to move the snake left  which = 37
+let snakeMoveRight = 0; // which = 39
+let snakeMoveUp = 0; //which =38
+let snakeMoveDown = 0; // which = 40
 
 window.onload = function () { // function to paint canvas and set interval
   const canvas = document.getElementById('game-canvas');
   const canvasContext = canvas.getContext('2d')
 
-  setInterval(draw, 1000)
+
+
+  setInterval(drawEverything, 50)
 }
 
-function draw() {
+function moveTheSnake() {
+  snakeMotion += 5
+}
+
+function drawEverything() {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);// these canvasContext are describing and moving the snake to the right 
   canvasContext.beginPath();
   canvasContext.rect(snakeMotion, 20, 60, 20);
   canvasContext.fillStyle = "#FF0000";
   canvasContext.fill();
   canvasContext.closePath();
-  snakeMotion += 20
 
-  canvas.addEventListener('keydown.which: 37', function (e) { // me trying to make an eventListener to make the snake go left, which: 37 is the left arrow key
-    if (snakeMoveLeft === false) {
+
+  document.addEventListener('keydown', function (e) { // me trying to make an eventListener to make the snake go left, which: 37 is the left arrow key
+    if(e.which === 37) {
+      snakeMotion = -10
       snakeMotion -= 20
     }
-  })
-
+   })
 }
