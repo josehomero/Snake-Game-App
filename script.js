8d666cd0fdb6ef6dab08a9cd28f1d6149e3d6cce
josehomero <jose.homero.rios@outlook.com>
2020-05-31 16:22:50 -0700

    working on making the apple appear randomly

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById('game-canvas');
 const canvasContext = canvas.getContext('2d')
 canvas.style.marginLeft = '30%';
-canvas.style.background = 'gray';
-let snakeHeadX = 255; // to move the snake on the x-axis
-let snakeHeadY = 250; // me attempting to move the snake left  which = 37
+canvas.style.background = 'LightGreen';
+let snakeHeadX = 220; // to move the snake on the x-axis
+let snakeHeadY = 240; // me attempting to move the snake left  which = 37
 let snakeSpeedHorizontal = 0;
 let snakeSpeedVertical = 0;
 
@@ -18,30 +18,52 @@ window.onload = function () { // function to paint canvas and set interval
 function moveTheSnake() {
   snakeHeadX = snakeHeadX + snakeSpeedHorizontal;
   snakeHeadY = snakeHeadY + snakeSpeedVertical;
+  //if snakeHead goes beyond canvas.width
+  //if snakHead goes beyond canvas.height
+  // if snakeHead goes beyond
+  if (snakeHeadX > canvas.width - 60) {
+    alert('game over');
+    return;
+  }
+
+  if (snakeHeadX < 0) {
+    alert('game over')
+    return;
+  }
+
+  if (snakeHeadY > canvas.height - 20) {
+    alert('game over');
+    return;
+  }
+
+  if (snakeHeadY < 0) {
+    alert('game over');
+    return;
+  }
 }
 
 document.addEventListener('keydown', function (e) { // me trying to make an eventListener to make the snake go left, which: 37 is the left arrow key
   if(e.which === 37) { // left arrow
-    snakeSpeedHorizontal = -15
+    snakeSpeedHorizontal = -20
     snakeSpeedVertical = 0;
     return;
   }
 
   if (e.which === 39) { // right arrow
-    snakeSpeedHorizontal = 15
+    snakeSpeedHorizontal = 20
     snakeSpeedVertical = 0;
     return;
   }
 
   if(e.which === 38) {// up arrow
     snakeSpeedHorizontal = 0;
-    snakeSpeedVertical = -15;
+    snakeSpeedVertical = -20;
     return;
   }
 
   if (e.which === 40) { // down arrow
     snakeSpeedHorizontal = 0;
-    snakeSpeedVertical = 15;
+    snakeSpeedVertical = 20;
     return;
   }
 
@@ -51,11 +73,11 @@ function drawEverything() {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);// these canvasContext are describing and moving the snake to the right 
   canvasContext.beginPath();
   canvasContext.rect(snakeHeadX, snakeHeadY, 60, 20);
-  canvasContext.fillStyle = "#FF0000";
+  canvasContext.fillStyle = "skyblue";
   canvasContext.fill();
   canvasContext.closePath();
 
-  canvasContext.fillStyle = 'red';
+  canvasContext.fillStyle = 'lightCoral';
   canvasContext.fillRect(20, 80, 20, 20)
 
 }
